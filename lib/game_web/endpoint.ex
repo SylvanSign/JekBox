be220351967ca871aa5601b640f4a860eedd5567
sylvansign <jake.d.bray@gmail.com>
2020-06-13 16:06:59 -0400

    gzip

--- lib/game_web/endpoint.ex
+++ lib/game_web/endpoint.ex
@@ -19,7 +19,7 @@ defmodule GameWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :game,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
