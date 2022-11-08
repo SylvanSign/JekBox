5d89e225049207b7b773ca93357c8039b4ca9747
kac460 <kac460@rutgers.edu>
2022-11-07 22:27:35 -0500

    Update deps to deal with issues

--- mix.exs
+++ mix.exs
@@ -33,12 +33,12 @@ defmodule Game.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.3"},
-      {:phoenix_live_view, "~> 0.13"},
+      {:phoenix, "~> 1.6.15"},
+      {:phoenix_live_view, "~> 0.18"},
       {:floki, ">= 0.0.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 3.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.7"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
